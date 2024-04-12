b259ced8e8a3005496bc19f6690839c98df82b7a
TheTalkingMime <talkingmime1@gmail.com>
2024-04-12 17:51:57 -0400

    made sheets an object

--- src/sheets.py
+++ src/sheets.py
@@ -1,28 +1,28 @@
 import gspread, os
 from itertools import chain
 
-def load_credentials(settings):
-    gc = gspread.service_account(filename= os.path.join(settings['cwd'], 'settings', 'credentials.json'))
-    conn = gc.open_by_url(settings['spreadsheet-link'])
-    return conn
+class SheetsManager:
+    def __init__(self, settings):
+        gc = gspread.service_account(filename= os.path.join(settings['cwd'], 'settings', 'credentials.json'))
+        self.conn = gc.open_by_url(settings['spreadsheet-link'])
 
-def format_to_spreadsheet(completed_objs, mapping):
-    data = []
-    for adv in list(completed_objs):
-        if adv not in mapping:
-            continue
-        data.append({
-            'range': mapping[adv],
-            'values': [[True]]
-        })
-    return data
+    def format_to_spreadsheet(self, completed_objs, mapping):
+        data = []
+        for adv in list(completed_objs):
+            if adv not in mapping:
+                continue
+            data.append({
+                'range': mapping[adv],
+                'values': [[True]]
+            })
+        return data
 
-def push_to_gsheet(conn, completed_objs, mapping, sheet_name):
-    data = format_to_spreadsheet(completed_objs, mapping)
-    conn.worksheet(sheet_name).batch_update(data)
+    def push_to_gsheet(self, completed_objs, mapping, sheet_name):
+        data = self.format_to_spreadsheet(completed_objs, mapping)
+        self.conn.worksheet(sheet_name).batch_update(data)
 
-def get_mappings(conn, sheet_name, spreadsheet_format):
-    sheet = conn.worksheet(sheet_name)
-    adv_ordering = list(chain.from_iterable(sheet.get(spreadsheet_format['ADV_RANGE'])))
-    adv_mapping = {item:f"{spreadsheet_format['ADV_COMPLETION_COL']}{i+2}" for i, item in enumerate(adv_ordering)}
-    return adv_mapping
+    def get_mappings(self, sheet_name, spreadsheet_format):
+        sheet = self.conn.worksheet(sheet_name)
+        adv_ordering = list(chain.from_iterable(sheet.get(spreadsheet_format['ADV_RANGE'])))
+        adv_mapping = {item:f"{spreadsheet_format['ADV_COMPLETION_COL']}{i+2}" for i, item in enumerate(adv_ordering)}
+        return adv_mapping
