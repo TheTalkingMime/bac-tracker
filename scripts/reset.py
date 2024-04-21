722b78a01f26c22f5d35a3cf658e390f32acecc2
TheTalkingMime <talkingmime1@gmail.com>
2024-04-21 01:15:15 -0400

    Updated reset script to read from settings

--- scripts/reset.py
+++ scripts/reset.py
@@ -19,9 +19,6 @@ def get_sheet_info(sheet_type, conn):
     }
 
 
-ADV_RANGE = "E2:E"
-ITEMS_RANGE = "C2:C"
-
 cwd = Path(__file__).resolve().parent.parent
 
 with open(os.path.join(cwd, "settings", "settings.json")) as f:
@@ -39,8 +36,8 @@ itemsSheet = items_sheet_info['worksheet']
 adv_sheet_format = advancement_sheet_info['format']
 items_sheet_format = items_sheet_info['format']
 
-adv_len = len(list(chain.from_iterable(advancementsSheet.get(ADV_RANGE))))
-item_len = len(list(chain.from_iterable(itemsSheet.get(ITEMS_RANGE))))
+adv_len = len(list(chain.from_iterable(advancementsSheet.get(adv_sheet_format['id_range']))))
+item_len = len(list(chain.from_iterable(itemsSheet.get(items_sheet_format['id_range']))))
 
 adv_vals = [{
                 'range': adv_sheet_format['status_range'],
@@ -53,8 +50,16 @@ adv_vals = [{
             {
                 'range': adv_sheet_format['when_range'],
                 'values': [[""] for x in range(adv_len)]
+            },
+            {
+                'range': adv_sheet_format['progress_range'],
+                'values': [[""] for x in range(adv_len)]
+            },
+            {
+                'range': adv_sheet_format['incomplete_range'],
+                'values': [[""] for x in range(adv_len)]
             }]
-item_vals = [[False] for x in range(item_len)]
+item_vals = [[False] for _ in range(item_len)]
 
 advancementsSheet.batch_update(adv_vals)
-itemsSheet.update(item_vals, ITEMS_RANGE)
+itemsSheet.update(item_vals, items_sheet_format['id_range'])
