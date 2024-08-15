d1d11c3550c71261c2874a6d5d921bf5131667bf
TheTalkingMime <talkingmime1@gmail.com>
2024-08-15 02:32:47 -0400

    Added support for retrying stats on failur

--- src/stats.py
+++ src/stats.py
@@ -3,6 +3,7 @@ import json
 import os
 from logging_config import LOGGING_CONFIG
 import logging
+from utils import retry_on_exception
 
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
@@ -26,19 +27,25 @@ class Statistics:
 
         for file in stats_files:
             uuid = file[:-5]
-            with open(os.path.join(self.statistics_path, file)) as f:
-                current_file = json.load(f)
+            
+            current_stats = self.read_stats_file(file)
 
-                for statistic in self.statistics:
-                    split = statistic.split(".")
-                    s = ["stats", f"minecraft:{split[0]}", f"minecraft:{split[1]}"]
-                    if s[0] in current_file and s[1] in current_file[s[0]] and s[2] in current_file[s[0]][s[1]]:
-                        if self.statistics[statistic] == "max" and current_file[s[0]][s[1]][s[2]] > stats[statistic]["value"]:
-                            stats[statistic]["value"] = current_file[s[0]][s[1]][s[2]]
-                            stats[statistic]["player"] = uuid
-                        if self.statistics[statistic] == "sum":
-                            stats[statistic]["value"] += current_file[s[0]][s[1]][s[2]]
+            for statistic in self.statistics:
+                split = statistic.split(".")
+                s = ["stats", f"minecraft:{split[0]}", f"minecraft:{split[1]}"]
+                if s[0] in current_stats and s[1] in current_stats[s[0]] and s[2] in current_stats[s[0]][s[1]]:
+                    if self.statistics[statistic] == "max" and current_stats[s[0]][s[1]][s[2]] > stats[statistic]["value"]:
+                        stats[statistic]["value"] = current_stats[s[0]][s[1]][s[2]]
+                        stats[statistic]["player"] = uuid
+                    if self.statistics[statistic] == "sum":
+                        stats[statistic]["value"] += current_stats[s[0]][s[1]][s[2]]
 
         for stat in stats:
             logger.debug(f"{stat}: {stats[stat]}")
         return stats
+    
+    @retry_on_exception(json.decoder.JSONDecodeError)
+    def read_stats_file(self, filename):
+        with open(os.path.join(self.statistics_path, filename)) as f:
+            current_stats = json.load(f)
+        return current_stats
