c7e88d36e62c39b807f3f6294b7b878f9016088d
TheTalkingMime <talkingmime1@gmail.com>
2024-08-15 02:33:07 -0400

    Added support for multiple exceptions

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,9 @@ from functools import wraps
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
 
-def retry_on_exception(exception_type, retries=3, delay=1):
+def retry_on_exception(exception_types, retries=3, delay=1):
+    if isinstance(exception_types, type):
+        exception_types = (exception_types,)
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -14,14 +16,14 @@ def retry_on_exception(exception_type, retries=3, delay=1):
             while n < retries:
                 try:
                     return func(*args, **kwargs)
-                except exception_type as e:
+                except exception_types as e:
                     n += 1
                     args_str = ", ".join(map(str, args))
                     kwargs_str = ", ".join(f"{k}={v}" for k, v in kwargs.items())
                     logger.warning(f"{func.__name__}({args_str}, {kwargs_str}) failed on attempt # {n}")
                     time.sleep(delay)
                     if n >= retries:
-                        logger.error(f"Failed after {retries} retries with {delay} second delay. Error: {e}")
+                        logger.warning(f"Failed after {retries} retries with {delay} second delay. Error: {e}")
                         raise
         return wrapper
     return decorator
