ad4c85d900469204a644561efcfd1e015442c554
TalkingMime <talkingmime1@gmail.com>
2024-11-20 17:48:01 -0500

    Reformatted local output to work without db

--- tracker/src/local_output.py
+++ tracker/src/local_output.py
@@ -5,20 +5,24 @@ import json
 
 class LocalOutput():
     def __init__(self, cwd):
-        self.json_db = self.initialize_db(cwd)
+        self.file_path = os.path.join(cwd, "data", "data.json")
+        self.skeleton_path = os.path.join(cwd, "data", "skeleton.json")
         
     # Creates a new json DB by copying the skeleton and overwriting previous DB
-    def initialize_db(self, cwd):
-        with open(os.join(cwd, "data", "skeleton.json"), "r") as f:
-            skeleton_data = json.load(f)
-        with open(os.join(cwd, "data", "data.json"), "w") as f:
-            json.dump(skeleton_data, f)
-        return db.getDb(os.join(cwd, "data", "data.json"))
+    def load_data(self, file_path):
+        with open(file_path, "r") as f:
+            return json.load(f)
+        
+    def write_data(self, file_path, data):
+        with open(file_path, "w") as f:
+            return json.dump(data, f)
 
     def update_db(self, adv_data):
-        for adv, info in adv_data.items():
-            current_data = self.json_db.getById(adv)
-            current_data["progress"] = info[0]
-            current_data["completed"] = info[3]
-            current_data["incomplete"] = info[2]
-            self.json_db.updateById(adv, current_data)
+        data = self.load_data(self.skeleton_path)
+        for key in data:
+            if key in adv_data:
+                info = adv_data[key]
+                data[key]["progress"] = info[0]
+                data[key]["completed"] = info[3]
+                data[key]["incomplete"] = info[2]
+        self.write_data(self.file_path, data)
